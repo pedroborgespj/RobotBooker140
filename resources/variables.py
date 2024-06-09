645a398b1ed8daf34639156553560f4eb739ea3d
Pedro Borges <pedroborgespj@gmail.com>
2024-06-09 13:03:15 -0300

    Get Booking Id e Get Booking methods

--- resources/variables.py
+++ resources/variables.py
@@ -1,3 +1,9 @@
 # Variaveis
 url = 'https://restful-booker.herokuapp.com'
 content_type = 'application/json'
+firstname = 'Bob'
+lastname = 'Marley'
+totalprice = 120
+depositpaid = False
+bookingdates = dict(checkin='2024-04-27', checkout='2024-04-27')
+additionalneeds = 'Breakfast'
